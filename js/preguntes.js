b238ad54083abba58a2bfa3103825e78869cdb36
ismabarroso <ismaelbarroso2001@ginebro.cat>
2020-12-14 18:33:06 +0100

    canvis

---
+++ js/preguntes.js
@@ -0,0 +1,20 @@
+let arrRespostes = [], arrRespostesCorrectes = [];
+arrRespostesCorrectes=[1,3,4,2];
+
+
+function pregunta1(objecte, pregNum){
+    console.log("pos = 0 " + objecte.form[0].checked);
+    console.log("pos = 1 " + objecte.form[1].checked);
+    console.log("pos = 2 " + objecte.form[2].checked);
+    console.log("pos = 3 " + objecte.form[3].checked);
+    let resposta;
+    for (let i=0; i<4; i++ ){
+        if (objecte.form[i].checked) {
+            resposta = i +1;
+        }
+    }
+arrRespostes[pregNum] = resposta;
+
+alert("Has escollit resposta" + arrRespostes[pregNum]);
+
+}
